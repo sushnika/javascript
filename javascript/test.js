d2fa9b20e9eb467f26aafbe9c3396624d7d8ee3b
sushnika <49842506+sushnika@users.noreply.github.com>
2023-09-06 17:37:43 +0000

    varibale added in test file

--- javascript/test.js
+++ javascript/test.js
@@ -1,1 +1,8 @@
 console.log('hello');
+const accountId = 133;
+let accountEmail = "sush@admin.com";
+var accoundPassword ="123456";
+accountCity = "Jalandhar";
+
+accountEmail = "newadmin@admin.com";
+console.table([accountId, accountEmail, accountCity]);
