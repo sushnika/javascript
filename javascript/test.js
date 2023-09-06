01b73be0a94c53fded07bb6f0c16e70371e61b86
sushnika <49842506+sushnika@users.noreply.github.com>
2023-09-06 17:38:57 +0000

    added message for using let

--- javascript/test.js
+++ javascript/test.js
@@ -4,5 +4,6 @@ let accountEmail = "sush@admin.com";
 var accoundPassword ="123456";
 accountCity = "Jalandhar";
 
+/*  try to use the let not var due to scope issue as var will change the value for some of varibales */
 accountEmail = "newadmin@admin.com";
 console.table([accountId, accountEmail, accountCity]);
