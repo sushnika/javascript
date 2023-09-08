47b3119f8e448d92a45c603ca207392dab57a37a
sushnika <49842506+sushnika@users.noreply.github.com>
2023-09-08 07:35:00 +0000

    datatype

---
+++ javascript/datatype.js
@@ -0,0 +1,7 @@
+"use strict" // treat all js code to use newer version 
+let num =123456;
+let text = "sushnika";
+// alert(3+3); // not suitalbe for node js works in browser
+console.log(typeof num);
+console.table([typeof num, typeof text]);
+console.log(num);
