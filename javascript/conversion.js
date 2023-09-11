41df2db49cd398197154af5a19e58d8caf6879fc
sushnika <49842506+sushnika@users.noreply.github.com>
2023-09-11 09:43:39 +0000

    Conversion of datatype

---
+++ javascript/conversion.js
@@ -0,0 +1,10 @@
+let number = undefined;
+console.log(typeof number);
+let valInNumber = Number(number);
+console.log(typeof valInNumber);
+console.log(valInNumber);
+
+// null = 0 
+// undefined = NaN(Not a Number)
+
+// we can convert boolean and String too and check the typeof after the conversion 
