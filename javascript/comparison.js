9195ca2c0af920498a7680477290b1cacae7ba77
sushnika <49842506+sushnika@users.noreply.github.com>
2023-09-13 05:03:45 +0000

    Comapriosn of Datatype

---
+++ javascript/comparison.js
@@ -0,0 +1,24 @@
+//let's Compare the DataTypes 
+
+// these are simple comparison and will result us in true or false 
+// console.log(2 > 1)
+// console.log(3 > 2)
+// console.log(3 > 2)
+
+//Important thing what if we compare the null (Please run in your system too check too)
+
+// console.log(null > 0) 
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// when you run these Null you will get a output like false, false, true and you may be confused but the reason is 
+
+// equality check == and comparison check <> <= >= works differently
+// comparison converts null to a number and null is treating as 0 in the third console
+
+// strict check === checks the datatype value too
+
+console.log("2" == 2);
+console.log("2" === 2)
+
+// Run the equaity check and see the difference.
