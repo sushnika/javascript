01af0dc275f300207e36ad1f49b7838f488d8bb0
sushnika <49842506+sushnika@users.noreply.github.com>
2023-09-13 06:15:10 +0000

    datatype Summary

---
+++ javascript/datatype-summary.js
@@ -0,0 +1,24 @@
+// primitive datatype (7 type categories call by value(copy mai changes))
+//String Number Boolean null(means empty) undefined, Symbol(uniqueness), bigInt
+
+let name ="sushnika";
+let age ="28";
+let logIn = false;
+let temp= null;
+let salary;
+let id = Symbol(123);
+let largeNum = 222238888888222n;
+
+
+// Non primitive & Reference Type 
+// Array ,Objects, Functions
+let color =['red','yellow','orange'];
+let list =
+{
+    name : "sush",
+    age : "28",
+}
+function hello(){
+    console.log('hello world');
+}
+hello();
