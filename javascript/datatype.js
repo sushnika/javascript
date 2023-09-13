4e83524053a0524e4a77f0634117348e23b1f464
sushnika <49842506+sushnika@users.noreply.github.com>
2023-09-13 05:07:53 +0000

    added comments

--- javascript/datatype.js
+++ javascript/datatype.js
@@ -2,6 +2,7 @@
 let num =123456;
 let text = "sushnika";
 // alert(3+3); // not suitalbe for node js works in browser
+
 console.log(typeof num);
 console.table([typeof num, typeof text]);
 console.log(num);
